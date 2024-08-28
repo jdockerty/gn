0e77175b0b9df905583f91cc0585ca6fa38969c4
Jack Dockerty <jdockerty19@gmail.com>
2024-08-28 21:20:12 +0100

    refactor: rejig output buffer to log lines

--- src/server.rs
+++ src/server.rs
@@ -4,26 +4,25 @@ use tokio::{io::AsyncReadExt, net::TcpListener};
 
 pub struct Server<W: Write> {
     addr: SocketAddr,
-    out: W,
+    buffer: W,
 }
 
 impl<W: Write> Server<W> {
-    pub fn new(addr: SocketAddr, out: W) -> Self {
-        Self { addr, out }
+    pub fn new(addr: SocketAddr, buffer: W) -> Self {
+        Self { addr, buffer }
     }
 
     pub async fn serve(&mut self) -> crate::Result<()> {
         let bind = TcpListener::bind(self.addr).await?;
-        writeln!(self.out, "Listening on tcp://{}", bind.local_addr()?)?;
+        eprintln!("Listening on tcp://{}", bind.local_addr()?);
 
         while let Ok((mut stream, _addr)) = bind.accept().await {
             let mut s = String::new();
             match stream.read_to_string(&mut s).await {
-                Ok(_) => writeln!(self.out, "{s}")?,
-                Err(e) => writeln!(self.out, "Unable to read stream: {e}")?,
+                Ok(_) => writeln!(self.buffer, "{s}")?,
+                Err(e) => eprintln!("Unable to read stream: {e}"),
             }
         }
-
         unreachable!("This is a blocking call");
     }
 }
