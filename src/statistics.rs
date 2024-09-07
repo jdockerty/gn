535756f912b80a2a2b3c5f4728543f426809885d
Jack Dockerty <jdockerty19@gmail.com>
2024-09-07 14:09:06 +0100

    feat: use atomics in stats

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,11 +1,15 @@
-use std::time::Instant;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
+use std::{sync::atomic::AtomicU64, time::Instant};
+
+use atomic_float::AtomicF64;
 
 pub struct Statistics {
     start_time: Instant,
-    total_bytes: u64,
-    success_count: u64,
-    failure_count: u64,
-    throughput: f64,
+    total_bytes: Arc<AtomicU64>,
+    success_count: Arc<AtomicU64>,
+    failure_count: Arc<AtomicU64>,
+    throughput: Arc<AtomicF64>,
 }
 
 impl Default for Statistics {
@@ -18,44 +22,47 @@ impl Statistics {
     pub fn new() -> Self {
         Self {
             start_time: Instant::now(),
-            total_bytes: 0,
-            success_count: 0,
-            failure_count: 0,
-            throughput: 0.0,
+            total_bytes: Arc::new(AtomicU64::new(0)),
+            success_count: Arc::new(AtomicU64::new(0)),
+            failure_count: Arc::new(AtomicU64::new(0)),
+            throughput: Arc::new(AtomicF64::new(0.0)),
         }
     }
 
     /// Increment the total number of bytes written
-    pub fn increment_total(&mut self, inc: u64) {
-        self.total_bytes += inc
+    pub fn increment_total(&self, inc: u64) {
+        self.total_bytes.fetch_add(inc, Ordering::Release);
     }
 
     /// Increment the number of successful requests
-    pub fn record_success(&mut self) {
-        self.success_count += 1;
+    pub fn record_success(&self) {
+        self.success_count.fetch_add(1, Ordering::Release);
     }
 
     /// Increment the number of failed requests
     pub fn record_failure(&mut self) {
-        self.failure_count += 1;
+        self.failure_count.fetch_add(1, Ordering::Release);
     }
 
     pub fn successful_requests(&self) -> u64 {
-        self.success_count
+        self.success_count.load(Ordering::Relaxed)
     }
 
     pub fn success_percentage(&self) -> f64 {
-        (self.success_count as f64 / (self.success_count + self.failure_count) as f64) * 100.0
+        let success = self.success_count.load(Ordering::Acquire) as f64;
+        let failure = self.failure_count.load(Ordering::Relaxed) as f64;
+
+        (success / (success + failure)) * 100.0
     }
 
     /// Get the total number of bytes written
     pub fn total_bytes(&self) -> u64 {
-        self.total_bytes
+        self.total_bytes.load(Ordering::Acquire)
     }
 
     /// Get the total number of sent requests.
     pub fn request_count(&self) -> u64 {
-        self.success_count + self.failure_count
+        self.success_count.load(Ordering::Acquire) + self.failure_count.load(Ordering::Relaxed)
     }
 
     /// Retrieve the perceived bytes per second throughput that was written to
@@ -64,7 +71,9 @@ impl Statistics {
     /// NOTE: Owing to truncation from nanosecond precision to seconds, the
     /// produced throughput may not be accurate for low write counts.
     pub fn record_throughput(&mut self) {
-        self.throughput = self.total_bytes as f64 / self.start_time.elapsed().as_secs() as f64;
+        let throughput = self.total_bytes.load(Ordering::Acquire) as f64
+            / self.start_time.elapsed().as_secs() as f64;
+        self.throughput.store(throughput, Ordering::Relaxed);
     }
 
     pub fn elapsed(&self) -> u128 {
@@ -73,7 +82,7 @@ impl Statistics {
 
     /// Return the recorded throughput
     pub fn throughput(&self) -> f64 {
-        self.throughput
+        self.throughput.load(Ordering::Acquire)
     }
 }
 
